72ba67115b5ed5eaf531d9e81e615909f68282d5
Makoto.Nishimure <tkm.nishimure@gmail.com>
2020-06-01 00:47:14 +0900

    2次元配列を1次元配列に変換して計算するコードの削除

--- matmul/stra_main.cpp
+++ matmul/stra_main.cpp
@@ -1,5 +1,5 @@
-#include "mm.hpp"
 #include "mat_mul.hpp"
+#include "mm.hpp"
 #include "mm_opt.hpp"
 
 void genmat(int n, double **a, int k);
@@ -20,40 +20,12 @@ void genmat(int n, double **a, int k) {
   }
 }
 
-void genmat2vec(int n, double *a, int k) {
-  int i, j;
-  double d, aij;
-  int nk = n / k;
-  double nk1 = 1. / nk;
-  for (i = n - 1, j = 0; 0 <= i; --i, ++j) {
-    d = ((n - i) / (double)n) / (i + 1);
-    aij = d * ((nk / 2 - (i % nk)) * nk1 + 1);
-    a[j] = (0.1 * aij);
-  }
-  a[j - 1] += 1;
-  for (i = 1, j = n; i < n; ++i, ++j) {
-    d = ((n - i) / (double)n) / (i + 1);
-    aij = d * ((nk / 2 - (i % nk)) * nk1 + 1);
-    a[j] = (0.1 * aij);
-  }
-}
-
 void _genmat(int n, mat a, mat b) {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
       a[i][j] = ((i + j) * 100 / 7.0);
       b[i][j] = ((i + j) * 300 / 7.0);
     }
-  }
-}
-
-void _print_vec(int n, arr a) {
-  int size = 2 * n - 1;
-  for (int i = size - n; 0 <= i; --i) {
-    for (int j = i; j < i + n; ++j) {
-      printf("%.5lf ", a[j]);
-    }
-    cout << endl;
   }
 }
 
@@ -68,72 +40,41 @@ void diff(int size, mat a, mat b) {
   }
 }
 
-void diffmatvec(int n, mat a, arr b) {
-  int size = 2 * n - 1;
-  for (int i = size - n, s = 0; 0 <= i; --i, ++s) {
-    for (int j = i, t = 0; j < i + n; ++j, ++t) {
-      if (a[s][t] != b[j]) {
-        cout << "DIFF MAT AND ARR" << endl;
-        exit(1);
-      }
-    }
-  }
-}
-
 int main(int argc, char **argv) {
   int n = 1504;
   int N = 1504;
 
-  adds *mat1, *mat2, *mat3, *mat4, *ar1;
+  adds *mat1, *mat2, *mat3;
   mat1 = allocMat(N, N);
-  ar1 = allocMat(1, 2 * N - 1);
-
+  mat2 = allocMat(N, N);
   mat3 = allocMat(N, N);
 
-  mat2 = allocMat(N, N);
-  // mat4 = allocMat(N, N);
-
-  mat m1, m2, m3, m4;
+  mat m1, m2, m3;
   m1 = mat1->m;
-
+  m2 = mat2->m;
   m3 = mat3->m;
 
-  m2 = mat2->m;
-  // m4 = mat4->m;
-
-  arr a1;
-  a1 = ar1->a;
-
   set_zero(n, m3);
-  // set_zero(n, m4);
 
   struct timeval tp[2];
   int roop = 10;
   genmat(n, m1, 5);
   genmat(n, m2, 6);
-  genmat2vec(n, a1, 6);
+
   gettimeofday(tp, 0);
   for (int i = 0; i < roop; ++i) {
     // matMul(n, m1, m2, m3);
-    // naive_ma(n, m1, a1, m3);
-    // block_ma(n, m1, a1, m3);
     str(0, n, m1, m2, m3);
-    // matarrMul(n, m1, a1, m3);
-    // matarrMul_b(n, m1, a1, m3);
-    // matarrMul_l(n, m1, a1, m3);
-    // matarrMul_t(n, m1, a1, m3);
     // thr_Mul(n, m1, m2, m3);
   }
   gettimeofday(tp + 1, 0);
-  // cout << "STR DONE" << endl;
+
   double one_exe_time = double(elapsed_time(tp)) / roop;
   printf("TIME : %lf\n", one_exe_time);
-  // diff (n, m3, m4);
-  // print_mat(n, m3);
+
   freeMat(mat1);
+  freeMat(mat2);
   freeMat(mat3);
 
-  freeMat(mat2);
-  // freeMat(mat4);
   return 0;
 }
